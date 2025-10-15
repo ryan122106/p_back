ee73f6f65f9c278514dd7fc59c1e8642116e2fb8
Ryan <ryan.sensei21@gmail.com>
2025-10-16 04:07:15 +0800

    c

--- routes/image.js
+++ routes/image.js
@@ -6,18 +6,22 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
   filename: (req, file, cb) => {
     cb(null, Date.now() + "_" + file.originalname.replace(/\s+/g, "_"));
-  }
+  },
 });
 
 const upload = multer({ storage });
-
 
 router.post("/", upload.array("media", 10), async (req, res) => {
   try {
     if (!req.files || req.files.length === 0)
       return res.status(400).send({ message: "No files uploaded" });
 
-    const urls = req.files.map(file => file.path);
+    const src = file.startsWith("http")
+      ? file
+      : `${window.location.origin}/${file
+          .replace(/^\/+/, "")
+          .replace(/\\/g, "/")}`;
+
     res.status(200).send({ urls });
   } catch (error) {
     console.log(error);
